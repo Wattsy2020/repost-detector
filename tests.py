922eb11a15ed6b8d9808ff1d1682d14c2a18bab3
DESKTOP-USGLRBO\liamw <liam.watts271@gmail.com>
2018-06-04 17:56:47 +1000

    Fix first test

--- tests.py
+++ tests.py
@@ -3,6 +3,8 @@ import main
 
 
 def test_post_archive_coverage():
+    posts = sorted(main.posts, key=lambda item: item.date)
+
     posts_in_month = []
     cutoff_date = datetime(2017, 1, 1).timestamp()
     i = 0
@@ -10,7 +12,7 @@ def test_post_archive_coverage():
     while cutoff_date < datetime.today().timestamp():
         posts_in_month.append(0)
 
-        while i < len(main.posts) and main.posts[i].date < cutoff_date:
+        while i < len(posts) and posts[i].date < cutoff_date:
             posts_in_month[-1] += 1
             i += 1
 
