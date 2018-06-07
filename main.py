7adf34f2190075f8d56a3ec2677d0c14a5528e54
Liam Watts <35185935+Wattsy2020@users.noreply.github.com>
2018-06-07 13:15:56 +1000

    Production ready
    -Added reply function
    -Adjust time spent analysing a post based on the age of the post
    -Refactoring
    
    This version of the bot has been tested and could be deployed right now however it's accuracy could be greatly improved by using a proper reverse image search instead of using unreliable OCR.

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import os
 import shutil
 import time
+import praw
 import prawcore
 from datetime import datetime
 
 import post_recorder
+import message
 from post_recorder import subreddit
 from post_comparison import Post, NewPost, compare_keywords
 
 
-def record_all_posts():
+def record_all_posts(posts_per_month):
     start_month = int(datetime(2017, 1, 1).timestamp())
     end_month = int(datetime(2017, 2, 1).timestamp())
 
     while end_month < datetime.today().timestamp():
-        post_recorder.record_old_posts(start_month, end_month, 1000)
+        post_recorder.record_old_posts(start_month, end_month, posts_per_month)
 
         # this actually adds 30 days instead of 1 month but that doesn't matter much
         start_month = end_month
         end_month = end_month + 2592000
 
     # record the most recent posts using praw
-    post_recorder.record_new_posts('month', 1000)
+    post_recorder.record_new_posts('month', posts_per_month)
 
 
 def load_all_posts():
@@ -42,15 +44,23 @@ def load_all_posts():
 def update_posts():
     post_recorder.record_new_posts('day', 35)
 
-    # it may seem overkill to reload every single post but it doesn't take that long compared to check_if_repost
+    # this is performed during a low traffic time so performance is not an issue
     return load_all_posts()
 
 
 def reply(repost: NewPost, original):
-    message = '{} is a repost of {}'.format(repost.link, original.link)
-    print('\n'+message)
     with open(output_storage_file, 'a+') as storage:
-        storage.write(message+'\n')
+        storage.write('{} is a repost of {}\n'.format(repost.link, original.link))
+
+    '''
+    reply_message = message.get_message(original.title, original.link)
+
+    # handle deleted posts
+    try:
+        repost.submission.reply(reply_message)
+    except praw.exceptions.APIException:
+        return
+    '''
 
 
 def check_if_repost(new_post, text_similarity_limit):
@@ -101,28 +111,33 @@ def check_if_repost(new_post, text_similarity_limit):
 
 
 def main():
-    while True:
-        process_start_time = datetime.today()
+    process_start_time = datetime.today()
 
-        for submission in subreddit.stream.submissions():
-            post = NewPost(submission, new_folder)
+    for submission in subreddit.stream.submissions():
+        post_age = datetime.today().timestamp() - submission.created_utc
+        if post_age > 3600: continue  # so the bot can be restarted without analysing a backlog of posts
+        print(post_age)
 
-            # handle internet dropouts
-            try:
-                check_if_repost(post, .2)
-            except prawcore.exceptions.ServerError:
-                time.sleep(180)
+        post = NewPost(submission, new_folder)
+        # can afford to spend more time analysing a post if we've analysed the rest of the submission stream
+        text_similarity_limit = .15 if post_age < 360 else .25
 
-            shutil.rmtree(new_folder)
+        # handle internet dropouts
+        try:
+            check_if_repost(post, text_similarity_limit)
+        except prawcore.exceptions.ServerError:
+            time.sleep(180)
 
-            # update in the middle of the day when there is low traffic
-            if process_start_time.day < datetime.today().day and datetime.today().hour == 12:
-                print('\n refreshing posts at {0:02d}'.format(datetime.today().hour)
-                      + ':{0:02d}'.format(datetime.today().minute))
+        shutil.rmtree(new_folder)
 
-                global posts
-                posts = update_posts()
-                process_start_time = datetime.today()
+        # update posts when there is low traffic (adjust hour depending on your timezone)
+        if process_start_time.hour < datetime.today().hour == 12:
+            print('\n refreshing posts at {0:02d}'.format(datetime.today().hour)
+                  + ':{0:02d}'.format(datetime.today().minute))
+
+            global posts
+            posts = update_posts()
+            process_start_time = datetime.today()
 
 
 posts = load_all_posts()
