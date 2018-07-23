4326c527bc82a5d8134f39f5d2a5a178f377ed7c
Wattsy2020 <liam.watts271@gmail.com>
2018-07-23 23:32:58 +1000

    Extract color histogram from image

---
+++ image_description.py
@@ -0,0 +1,51 @@
+import numpy as np
+import cv2
+
+
+class ColorDescriptor:
+    def __init__(self, bins):
+        self.bins = bins
+
+    # extracts a color histogram from a section of an image
+    def histogram(self, image, mask):
+        hist = cv2.calcHist([image], [0, 1, 2], mask, self.bins, [0, 180, 0, 256, 0, 256])
+        hist = cv2.normalize(hist, hist).flatten()
+
+        return hist
+
+    # extracts a color histogram from the image, analysing sections of the image separately for greater accuracy
+    def describe(self, image_path):
+        image = cv2.imread(image_path)
+
+        # convert the image to the HSV color space and initialize the features used to quantify the image
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        features = []
+
+        # grab the dimensions and compute the center of the image
+        h, w = image.shape[:2]
+        cx, cy = int(w * 0.5), int(h * 0.5)
+
+        # divide the image into four rectangular segments
+        segments = [(0, cx, 0, cy), (cx, w, 0, cy), (cx, w, cy, h), (0, cx, cy, h)]
+
+        # construct an elliptical mask representing the center of the image
+        x_radius, y_radius = int(w * 0.75) // 2, int(h * 0.75) // 2
+        ellipse_mask = np.zeros(image.shape[:2], dtype="uint8")
+        cv2.ellipse(ellipse_mask, (cx, cy), (x_radius, y_radius), 0, 0, 360, 255, -1)
+
+        # loop over the segments and create a color histogram for each
+        for (startX, endX, startY, endY) in segments:
+            # construct a mask for each corner of the image, subtracting the elliptical center from it
+            corner_mask = np.zeros(image.shape[:2], dtype="uint8")
+            cv2.rectangle(corner_mask, (startX, startY), (endX, endY), 255, -1)
+            corner_mask = cv2.subtract(corner_mask, ellipse_mask)
+
+            # extract a color histogram from the image and update the feature vector
+            hist = self.histogram(image, corner_mask)
+            features.extend(hist)
+
+        # extract a color histogram from the elliptical region and update the feature vector
+        hist = self.histogram(image, ellipse_mask)
+        features.extend(hist)
+
+        return features
